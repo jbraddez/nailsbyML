a85f813e1b8c05299f78b6f6b852e4703e8a563f
jbradleydev <jacobbradleydev@gmail.com>
2025-05-24 18:01:46 +0100

    added PWA' g

--- nails_beauty.js
+++ nails_beauty.js
@@ -1,10 +1,11 @@
 const serviceConts = document.querySelectorAll('.serviceCont');
 serviceConts.forEach(cont => {
-    const toggleArea = cont.querySelector('.name_arrow');
+    const nameArrow = cont.querySelector('.name_arrow');
+
     cont.addEventListener('click', (e) => {
-        if (!toggleArea.contains(e.target)) return;
-        const cards = cont.querySelectorAll('.card');
-        cards.forEach(card => {
+        if (!nameArrow.contains(e.target) && e.target !== cont) return;
+
+        cont.querySelectorAll('.card').forEach(card => {
             card.classList.toggle('hidden');
         });
     });
